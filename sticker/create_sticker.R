b53b9055ed3659634222bbe75f87c23f0f79129e
jgw17 <jweberpals@bwh.harvard.edu>
2023-01-09 22:20:06 -0500

    first commit sticker

---
+++ sticker/create_sticker.R
@@ -0,0 +1,46 @@
+# create sticker for package
+library(hexSticker)
+library(here)
+library(ggdag)
+library(tidyverse)
+
+img_path <- here("sticker", "smdi_logo.png")
+
+sticker(
+  img_path,
+  package = "smdi",
+  h_fill = "white",
+  h_color = "deepskyblue1",
+  #h_size = 5,
+  p_size = 8,
+  p_color = "black",
+  # p_x = ,
+  p_y = 1.5,
+  s_x = 1.05,
+  s_y= .8,
+  s_width=.5,
+  s_height =.4,
+  dpi = 1200,
+  filename="sticker/smdi_hexagon.png"
+  )
+
+
+img_path <- here("sticker", "smdi.tiff")
+
+sticker(
+  img_path,
+  package = "smdi",
+  h_fill = "white",
+  h_color = "deepskyblue1",
+  #h_size = 5,
+  p_size = 8,
+  p_color = "black",
+  # p_x = ,
+  p_y = 1.5,
+  s_x = 1.05,
+  s_y= .8,
+  s_width=.5,
+  s_height =.4,
+  dpi = 1200,
+  filename="sticker/smdi_hexagon2.png"
+)
