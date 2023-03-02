5e3adbc016c7cf9e44fffbe2aa2d657a2a3f7649
jgw17 <jweberpals@bwh.harvard.edu>
2023-03-02 11:12:53 -0500

    changed to two datasets: one complete and one with missings

--- R/smdi_data_complete.R
+++ R/smdi_data_complete.R
@@ -1,24 +1,24 @@
-#' smdi exemplary lung cancer dataset
+#' smdi exemplary lung cancer dataset (with complete data)
 #'
 #'
 #' Report ...
 #'
-#' @format ## `smdi_data`
+#' @format ## `smdi_data_complete`
 #' A data frame with 1,000 rows and 12 columns:
 #' \describe{
 #'   \item{exposure}{Treatment assignment variable (binary). Indicates initiation of the exposure of interest (1) versus a comparator regimen (0)}
 #'   \item{age_num}{Age at baseline in years}
 #'   \item{female_cat}{Is gender female (0 = no, 1 = yes)}
-#'   \item{ecog_cat}{ECOG performance score at baseline (0 versus 1). Shows 30% missingness following an MCAR mechanism.}
+#'   \item{ecog_cat}{ECOG performance score at baseline (0 versus 1)}
 #'   \item{smoking_cat}{Smoking status at baseline (0 = non-smoker, 1 = smoker)}
 #'   \item{physical_cat}{Physical activity at baseline (not active versus active)}
-#'   \item{egfr_cat}{EGFR mutation status (0 = wild-type, 1 = alteration). Shows 20% missingness following an MAR mechanism.}
+#'   \item{egfr_cat}{EGFR mutation status (0 = wild-type, 1 = alteration)}
 #'   \item{alk_cat}{ALK transolcation mutation status (0 = wild-type, 1 = alteration)}
-#'   \item{pdl1_num}{PD-L1 cell staining biomarker in %. Shows 40% missingness following an MNAR(value) mechanism}
+#'   \item{pdl1_num}{PD-L1 cell staining biomarker in %}
 #'   \item{histology_cat}{Tumor histology (0 = nonsquamous, 1 = squamous)}
 #'   \item{ses_cat}{Socio-economic status (multi-categorical: 1-low, 2-middle, 3-high)}
 #'   \item{copd_cat}{COPD comorbidity at baseline}
 #'   ...
 #' }
 #' @source <https://janickweberpals.gitlab-pages.partners.org/smdi/articles/data_generation.html>
-"smdi_data"
+"smdi_data_complete"
