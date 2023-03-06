94e14542028cd66754b0354d99979ae94cce9098
jgw17 <jweberpals@bwh.harvard.edu>
2023-03-06 16:50:11 -0500

    finalization of first draft, need to figure out generics

--- R/smdi_asmd.R
+++ R/smdi_asmd.R
@@ -1,19 +1,35 @@
-#' Compute mean/median absolute standardized mean differences
+#' Computes mean/median absolute standardized mean differences between observed and missing observations
 #'
 #' @description
-#' This function takes a dataframe and covariates which are only partially observed/missing.
-#' The dataframe should consist of the exposure variable, the outcome variable(s), the partially observed covariates
-#' and all other fully observed covariates which are deemed important for the final modeling
-#' or which could be considered as auxiliary variables. If no partially observed covariates are provided,
-#' the function automatically looks for all variables/columns with NA.
+#' This function takes a dataframe with covariates which are only partially observed/missing and returns the
+#' median/average absolute standardized mean difference (*asmd*) for every specified covariate in `covar`
+#' (if NULL all covariates with at least one NA are considered).
 #'
-#'Important: don't include variables like ID variables, ZIP codes, dates, etc.
+#' @details
+#' The *asmd* is computed for every covariate one-by-one and not jointly. If there is multivariate
+#' missingness, i.e. more than just one missing covariate exist, you can decide what should
+#' happen with the other partially observed 'predictor' covariates using the `includeNA` parameter.
+#' That is, if `includeNA` is set to FALSE (default), only the *asmd* between observed cases will be computed,
+#' and if `includeNA` is set to TRUE, missingness is modeled as an explicit category (categorical covariates only).
+#'
+#' If any other behavior is desired, data transformations for example with the smdi::smdi_na_indicator function, may make sense
+#' before calling the function.
+#'
+#' The dataframe should generally consist of the exposure variable, the outcome variable(s), the partially observed covariates
+#' and all other fully observed covariates which are deemed important for the final modeling
+#' and (optionally) which could be considered as auxiliary variables. If no partially observed covariates are provided,
+#' the function automatically looks for all variables/columns with NA (powered by the smdi::smdi_summarize() function)
+#'
+#'*Important*: don't include variables like ID variables, ZIP codes, dates, etc.
+#'
+#'Credit: Much of this function utilizes functionalities from tableone::CreateTableOne
 #'
 #' @param data dataframe or tibble object with partially observed/missing variables
-#' @param covar character covariate or covariate vector with variable/column name(s) to investigate. If NULL, the function automatically includes all columns with at least one missing observation
-#' @param na_strategy "retain" or "drop" covariates after creating misisng indicator variables; more info see ?smdi::smdi_na_indicator()
-#' @param median logical if the median (recommended default) or mean of all absolute standardized mean differences should be computed
+#' @param covar character covariate or covariate vector with variable/column name(s) to investigate. If NULL, the function automatically includes all columns with at least one missing observation and all remaining covariates will be used as predictors
+#' @param median logical if the median (= TRUE; recommended default) or mean of all absolute standardized mean differences (asmd) should be computed
+#' @param includeNA logical, should missingness of other partially observed covariates be explicitly modeled (default is FALSE)
 #' @param ... further arguments
+#'
 #' @return returns mean/median absolute standardized mean differences
 #'
 #' @importFrom magrittr '%>%'
@@ -47,73 +63,123 @@
 
 smdi_asmd <- function(data = NULL,
                       covar = NULL,
-                      na_strategy = c("retain", "drop"),
                       median = TRUE,
+                      includeNA = FALSE,
                       ...
                       ){
+
+
+  covariate <- `1 vs 2` <- NULL
 
   # additional arguments
   add_args <- list(...)
 
+  # pick missing indicator columns/partially observed covariates
+  # check for missing covariates
   covar_miss <- smdi::smdi_check_covar(
     data = data,
     covar = covar
     )
 
-
   # start applying smd computation over all partially observed covariates
   smd_loop <- function(i){
 
-    # create missing indicator
-    data_encoded <- data %>%
-      smdi::smdi_na_indicator(
-        na_strategy = na_strategy
+    # apply smdi_na_indicator for i to create missing
+    # indicator variable
+    strata_df <- smdi::smdi_na_indicator(
+      data = data,
+      covar = i,
+      drop_NA_col = TRUE
       )
+
+    # create strata variable
+    strata_var <- paste0(i, "_NA")
 
     # create tableone
     tbl1 <- tableone::CreateTableOne(
-        data = data_tmp,
-        vars = names(data_tmp %>% dplyr::select(-na_indicator)),
-        strata = "na_indicator",
+        data = strata_df,
+        # all covariates except strata covariate
+        vars = names(strata_df)[ !names(strata_df) == strata_var],
+        # strata covariate
+        strata = strata_var,
         # if multiple variables are missing, NA is an own category
-        includeNA = TRUE
+        includeNA = includeNA,
+        smd = TRUE
         )
 
-    smd <- tableone::ExtractSmd(tbl1)
-
+    # extract smd and compute median/mean
+    smd <- tableone::ExtractSmd(tbl1) %>%
       as.data.frame() %>%
       tibble::rownames_to_column(var = "covariate") %>%
+      dplyr::filter(covariate != i) %>%
       dplyr::rename(smd = `1 vs 2`)
+
+    # asmd plot
+    asmd_plot <- smd %>%
+      ggplot2::ggplot(
+        ggplot2::aes(
+          y = forcats::fct_reorder(covariate, smd),
+          x = smd,
+          color = ifelse(smd < 0.1, "darkgreen", "firebrick")
+          )
+        ) +
+      ggplot2::geom_point(size = 3) +
+      ggplot2::labs(
+        title = glue::glue("ASMD plot for covariate '{i}'"),
+        x = "Absolute standardized mean difference [ASMD]",
+        y = "",
+        color = "asmd < 0.1",
+        caption = glue::glue("ASMD is computed as the asmd between patients with and without observed '{i}'")
+        ) +
+      ggplot2::scale_color_identity() +
+      ggplot2::theme_bw()
 
     if(isTRUE(median)){
 
-      smd_summary <- tibble::tibble(
+      asmd_aggregate <- tibble::tibble(
         covariate = paste(i),
-        smd_median = stats::median(smd_tmp[,1])
+        asmd_median = stats::median(smd$smd)
         )
 
     }else{
 
-      smd_summary <- tibble::tibble(
+      asmd_aggregate <- tibble::tibble(
         covariate = paste(i),
-        smd_mean = mean(smd_tmp[,1])
+        asmd_mean = mean(smd$smd)
         )
 
     }
 
-    return(smd_summary)
-
-  }
-
-  smd_list <- lapply(covar_miss$covariate, FUN = smd_loop)
-
-  smd_return <- covar_miss_all %>%
-    dplyr::left_join(
-      do.call(rbind, smd_list),
-      by = "covariate"
+    # assemble lapply output object
+    return(
+      list(
+        asmd_covar = i,
+        asmd_table1 = print(tbl1, smd = TRUE, printToggle = FALSE),
+        asmd_plot = asmd_plot,
+        asmd_aggregate = asmd_aggregate
+        )
       )
 
-  return(smd_return)
+    }
+
+  # iterate above analyses overall specified
+  # partially observed covariates
+  asmd_out <- lapply(covar_miss, FUN = smd_loop)
+  names(asmd_out) <- covar_miss
+
+  class(asmd_out) <- "asmd"
+
+  return(asmd_out)
 
 }
+
+#' @export
+print.asmd <- function(object, ...){
+
+  cat("Summary ASMD table: \n")
+  df <- do.call(rbind, lapply(object,'[[',4))
+
+  return(df)
+}
+
 
