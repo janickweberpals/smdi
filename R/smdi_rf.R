1c5c2a92c329569d1a4d9140e9215d8c8bad05a0
jgw17 <jweberpals@bwh.harvard.edu>
2023-03-07 23:26:39 -0500

    first draft of smdi_rf and re-build of website

---
+++ R/smdi_rf.R
@@ -0,0 +1,163 @@
+#' Computes random forest-based AUC
+#'
+#' @description
+#' The function trains and fits a random forest model to assess the ability to predict missingness for
+#' the specified covariate(s). If missing indicator can be predicted as a function of observed covariates,
+#' MAR may be a likely scenario and would imply that imputation may be feasible.
+#'
+#'#' #' Important: don't include variables like ID variables, ZIP codes, dates, etc.
+#'
+#' @details
+#' The random forest utilizes the randomForest engine.
+#'
+#' @references
+#' 	Weberpals J, Sondhi A., et al.
+#' 	[Pharmacoepidemiol Drug Saf, Vol. 30, pp. 36-36, 2021](https://onlinelibrary.wiley.com/doi/10.1002/pds.5305),
+#' 	presented as an oral presentation at the International Conference on
+#' 	Pharmacoepidemiology and Risk Management (2021)
+#'
+#' @param data dataframe or tibble object with partially observed/missing variables
+#' @param covar character covariate or covariate vector with partially observed variable/column name(s) to investigate. If NULL, the function automatically includes all columns with at least one missing observation and all remaining covariates will be used as predictors
+#' @param ntree integer, number of trees (defaults to 1000 trees)
+#' @param train_test_ratio numeric vector to indicate the test/train split ratio, e.g. c(.7, .3) which is the default
+#' @param set_seed seed for reproducibility, defaults to 42
+#' @param ... additional arguments
+#'
+#' @return rf object: list that contains the ROC AUC value and corresponding variable importance in training dataset (latter as ggplot object)
+#'
+#' @importFrom dplyr mutate
+#' @importFrom dplyr select
+#' @importFrom forcats fct_reorder
+#' @importFrom ggplot2 aes
+#' @importFrom ggplot2 coord_flip
+#' @importFrom ggplot2 geom_point
+#' @importFrom ggplot2 ggplot
+#' @importFrom ggplot2 labs
+#' @importFrom ggplot2 scale_color_identity
+#' @importFrom ggplot2 theme_bw
+#' @importFrom glue glue
+#' @importFrom naniar mcar_test
+#' @importFrom pROC roc
+#' @importFrom randomForest randomForest
+#' @importFrom stats predict
+#' @importFrom tibble tibble
+#' @importFrom tibble rownames_to_column
+#' @importFrom tidyselect all_of
+#'
+#' @export
+#'
+#' @examples
+#'\dontrun{
+#' library(smdi)
+#' library(dplyr)
+#'
+#' smdi_data %>%
+#'  smdi_little()
+#'
+#' }
+
+smdi_rf <- function(data = NULL,
+                    covar = NULL,
+                    train_test_ratio = c(.7, .3),
+                    set_seed = 42,
+                    ntree = 1000,
+                    ...
+                    ){
+
+  # initialize
+  .data <- MeanDecreaseAccuracy <- V1 <- covariate <- NULL
+
+  # additional arguments
+  add_args <- list(...)
+
+  # pre-checks
+  if(is.null(data)){stop("No dataframe provided.")}
+
+  # pick missing indicator columns/partially observed covariates
+  # check for missing covariates
+  covar_miss <- smdi::smdi_check_covar(
+    data = data,
+    covar = covar
+    )
+
+  # apply smdi_na_indicator for datset to create missing
+  # indicator variables
+  data_encoded <- smdi::smdi_na_indicator(
+    data = data,
+    covar = covar,
+    drop_NA_col = TRUE
+    )
+
+  # start applying smd computation over all partially observed covariates
+  rf_loop <- function(i){
+
+    # create strata variable
+    target_var <- paste0(i, "_NA")
+
+    # format missing indicator (target_var) variable correctly
+    data_encoded <- data_encoded %>%
+      dplyr::mutate(target_var = as.factor(.data[[target_var]])) %>%
+      dplyr::select(-tidyselect::all_of(target_var))
+
+    # use x% of dataset as training set and y% as test set
+    set.seed(set_seed)
+    sample <- sample(c(TRUE, FALSE), nrow(data_encoded), replace = TRUE, prob = train_test_ratio)
+    train  <- data_encoded[sample, ]
+    test   <- data_encoded[!sample, ]
+
+    set.seed(set_seed)
+    rf <- randomForest::randomForest(as.factor(target_var) ~ ., data = train, ntree = ntree, importance = TRUE)
+
+    # evaluate on test set
+    rf_test <- stats::predict(rf, newdata = test, type = "response", importance = T)
+
+    rf.roc <- pROC::roc(response = test$target_var, predictor = as.numeric(as.character(rf_test)), quiet = TRUE)
+    auc <- pROC::auc(rf.roc)[[1]]
+
+    rf_tbl_out <- tibble::tibble(
+      covariate = target_var,
+      rf_auc = auc
+      )
+
+    rf_plot_out <- rf$importance %>%
+      as.data.frame() %>%
+      tibble::rownames_to_column(var = "covariate") %>%
+      ggplot2::ggplot(ggplot2::aes(x = forcats::fct_reorder(as.factor(covariate), MeanDecreaseAccuracy), y = MeanDecreaseAccuracy)) +
+      ggplot2::geom_point(size = 5, color = "darkblue") +
+      ggplot2::labs(
+        x = "Covariate",
+        y = "Mean decrease in accuracy",
+        title = glue::glue("Covariate importance for {target_var} (training set)")
+      ) +
+      ggplot2::coord_flip() +
+      ggplot2::theme_bw()
+
+    rf_out <- list(
+      rf_table = rf_tbl_out,
+      rf_plot = rf_plot_out
+      )
+
+    return(rf_out)
+
+  }
+
+  # run the function for each covariate
+  rf_out <- lapply(covar_miss, FUN = rf_loop)
+  names(rf_out) <- covar_miss
+
+  # assign class
+  class(rf_out) <- "rf"
+
+  return(rf_out)
+
+}
+
+#' @export
+print.rf <- function(x, ...){
+
+  tbl <- do.call(rbind, lapply(x,'[[',1))
+  print(tbl)
+
+  return(tbl)
+
+}
