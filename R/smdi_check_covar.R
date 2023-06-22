2cacfb96925c54be6942974e4d08c1844f57de44
jgw17 <jweberpals@bwh.harvard.edu>
2023-06-22 10:41:36 -0400

    curly bracket indicating end of "if all covariates are present" statement was wrongly placed - fixed in this commit; improved documentation

--- R/smdi_check_covar.R
+++ R/smdi_check_covar.R
@@ -1,7 +1,7 @@
-#' This is a util function to help check input data and covariates provided
+#' This is a utility function to help check input data and covariates provided
 #'
 #' @param data dataframe or tibble object with partially observed/missing variables
-#' @param covar character covariate or covariate vector with partially observed variable/column name(s) to investigate. If NULL, the function automatically includes all columns with at least one missing observation
+#' @param covar character covariate or covariate vector with partially observed variable/column name(s) to investigate. If NULL, the function automatically looks for and includes all columns with at least one missing observation
 #'
 #' @return returns the covariate vector for subsequent tasks or warnings/errors
 #'
@@ -28,7 +28,7 @@ smdi_check_covar <- function(data = NULL,
   # if <covar> is specified:
   if(!is.null(covar)){
 
-    # give an error of not all covariates specified in <covar> are present in <data>
+    # give an error if not all covariates specified in <covar> are present in <data>
     if(any(!covar %in% colnames(data))){
 
       stop("Not all covariates specified in <covar> are present in the <data> provided.")
@@ -57,9 +57,10 @@ smdi_check_covar <- function(data = NULL,
 
           # return covariates specified in <covar> for subsequent operations
           covar_miss <- covar
-        }
 
-      }
+        } # end: all covariates are present and have at least one NA
+
+      } # end: if all covariates are present
 
     # if covar is not specified, i.e. is null
     }else{
@@ -69,14 +70,14 @@ smdi_check_covar <- function(data = NULL,
         dplyr::select(tidyselect::where(~sum(is.na(.x)) > 0)) %>%
         names()
 
+      } # end: if no <covar> are specified
+
     # give error if there are no covariates with at least one NA value
     if(length(covar_miss)==0){
 
       stop("Found no covariates with missing values. Please check that missing values are coded as <NA>.")
 
     }
-
-  }
 
   # return covariate vector
   return(covar_miss)
